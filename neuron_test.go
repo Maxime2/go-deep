362edec1bbe55cb5eb07e61ed7913b1ec2abaf59
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2023-03-10 12:29:36 +1100

    add synapse fire test

---
+++ neuron_test.go
@@ -0,0 +1,30 @@
+package deep
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Synapse_Fire(t *testing.T) {
+	degree := 7
+	s := &Synapse{
+		Weights: []Deepfloat64{277.9848903746018,
+			-46.494014333557395,
+			-52.90141723171177,
+			-53.09001803405063,
+			-53.10138885017573,
+			-53.10279737262996,
+			-53.10308684399308,
+			-53.10317433993435},
+		Weights_1:  make([]Deepfloat64, degree+1),
+		IsComplete: make([]bool, degree+1),
+		FakeRoot:   make([][]Deepfloat64, degree+1),
+		In:         7.9556310898783e-14,
+		Out:        0,
+		IsBias:     false,
+		Tag:        "test",
+	}
+	s.fire(7.9556310898783e-14)
+	assert.InEpsilon(t, float64(s.Out), 277.984890374598, 0.0000001)
+}
