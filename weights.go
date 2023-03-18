7dc63b3fd47a1264c3d922b244d8eb864fe996cb
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2023-03-19 08:56:37 +1100

    initialise RNG when creating new weigh initialiser

--- weights.go
+++ weights.go
@@ -1,12 +1,16 @@
 package deep
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 // A WeightInitializer returns a (random) weight
 type WeightInitializer func() Deepfloat64
 
 // NewUniform returns a uniform weight generator
 func NewUniform(stdDev, mean float64) WeightInitializer {
+	rand.Seed(time.Now().UnixNano())
 	return func() Deepfloat64 { return Deepfloat64(Uniform(stdDev, mean)) }
 }
 
@@ -18,6 +22,7 @@ func Uniform(stdDev, mean float64) float64 {
 
 // NewNormal returns a normal weight generator
 func NewNormal(stdDev, mean float64) WeightInitializer {
+	rand.Seed(time.Now().UnixNano())
 	return func() Deepfloat64 { return Deepfloat64(Normal(stdDev, mean)) }
 }
 
