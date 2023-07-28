7d637402e6d5f5f70051d4104cdb6a9987f1e8c3
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2023-07-29 07:53:24 +1000

    disable check for completion

--- training/trainer.go
+++ training/trainer.go
@@ -262,18 +262,18 @@ func (t *OnlineTrainer) update0(neural *deep.Neural, it int) int {
 						update = (synapse.Weights[k] + delta)
 
 						if !math.IsNaN(float64(update)) && !math.IsInf(float64(update), 0) {
-							if it > 2 {
-								if (update-synapse.Weights[k])/(1-(update-synapse.Weights[k])/(synapse.Weights[k]-synapse.Weights_1[k])) < deep.Eps {
-									//synapse.IsComplete[k] = true
-									Lcompleted++
-								} //else if math.Abs(float64(update-synapse.Weights[k]))/math.Abs(float64(synapse.Weights[k]-synapse.Weights_1[k])) > 1 {
-								//	if update > synapse.Weights[k] {
-								//		update = deep.Deepfloat64(math.Abs(float64(synapse.Weights[k]-synapse.Weights_1[k]))) - deep.Eps + synapse.Weights[k]
-								//	} else {
-								//		update = synapse.Weights[k] + deep.Eps - deep.Deepfloat64(math.Abs(float64(synapse.Weights[k]-synapse.Weights_1[k])))
-								//	}
-								//}
-							}
+//							if it > 2 {
+//								if (update-synapse.Weights[k])/(1-(update-synapse.Weights[k])/(synapse.Weights[k]-synapse.Weights_1[k])) < deep.Eps {
+//									//synapse.IsComplete[k] = true
+//									Lcompleted++
+//								} //else if math.Abs(float64(update-synapse.Weights[k]))/math.Abs(float64(synapse.Weights[k]-synapse.Weights_1[k])) > 1 {
+//								//	if update > synapse.Weights[k] {
+//								//		update = deep.Deepfloat64(math.Abs(float64(synapse.Weights[k]-synapse.Weights_1[k]))) - deep.Eps + synapse.Weights[k]
+//								//	} else {
+//								//		update = synapse.Weights[k] + deep.Eps - deep.Deepfloat64(math.Abs(float64(synapse.Weights[k]-synapse.Weights_1[k])))
+//								//	}
+//								//}
+//							}
 							synapse.Weights_1[k] = synapse.Weights[k]
 							synapse.Weights[k] = update
 							// re-fire synapse with updated weights
